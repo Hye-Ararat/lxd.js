d156e36f037fcb147566455c46ee3e9141d7307d
Joseph Maldjian <josephavm@icloud.com>
2023-03-30 17:54:40 -0500

    add instance listing and instance handler

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,15 @@
 import {Axios} from "axios";
+import Instance from "./instance";
+import {InstanceFull} from "./lib/lxd/instance";
+import { ResponseRaw } from "./lib/lxd/response";
 
 export default function client(requestClient: Axios) {
-    
+    return {
+        instances: async () => {
+            const request = await requestClient.get("/instances?recursion=2");
+            const response = request.data as ResponseRaw;
+            return response.metadata as InstanceFull[];
+        },
+        instance: (name: string) => Instance(name, requestClient)
+    }
 }
