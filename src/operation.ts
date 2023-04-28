1ef236986f2db2ddcc3bedebf0fa8c39cbeb80b3
Joseph Maldjian <josephavm@icloud.com>
2023-04-28 09:21:21 -0500

    operation type

--- src/operation.ts
+++ src/operation.ts
@@ -1,4 +1,6 @@
 import { Axios } from "axios";
+import { ResponseRaw } from "./lib/lxd/response";
+import {Operation as OperationType} from "./lib/lxd/opteration";
 
 export default class Operation {
     private id: string;
@@ -11,7 +13,7 @@ export default class Operation {
 
     async getState(){
         let request = await this.requestClient.get(`/operations/${this.id}`);
-        let response = request.data;
-        return response.metadata;
+        let response = request.data as ResponseRaw;
+        return response.metadata as Operation;
     }
 }
